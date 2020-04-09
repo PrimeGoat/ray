66a1947eb30144736b8cefa6bbb90e3ffd938353
DENIS SAVGIR <denis@DENISs-MBP.home>
2020-04-09 04:09:59 -0400

    More features

--- main.js
+++ main.js
@@ -16,19 +16,23 @@
  *   as numerically-sorted numerics followed by alphabetically-sorted non-numerics.
  * - Object.keys() returns only array indices, excludes methods.
  * - .Array() returns the contents as an Array object.
+ * - Iterator implemented so that array can be traversed using for-of
+ * - Can be expressed as a primitive.
  * 
  */
 
-// Set up internal array object to be handled by proxy
-const _Ray = function(arr) {
-    const out = {
-    length: 0,
 
+const Ray = function(arr = []) {
+  return new Proxy({
+    ...arr,
+
+    length: arr.length,
+  
     push: function(value) {
-      this[this.length] = value;
+      this[this.length] = value; // Adding new element automatically updates .length
       return this.length;
     },
-
+  
     pop: function() {
       const tmp = this[this.length - 1];
       this.length--; // Proxy will automatically truncate
@@ -41,7 +45,7 @@ const _Ray = function(arr) {
       }
       this[0] = value;
     },
-
+  
     shift: function() {
       tmp = this[0];
       for(let i = 1; i < this.length; i++) {
@@ -50,19 +54,21 @@ const _Ray = function(arr) {
       this.length--; // Proxy will automatically truncate
       return tmp;
     },
-
-    // .keys is always set to a list of numeric keys in the array.
-    // Length of list isn't always equal to .length.  Deleted elements stay as undefined
+  
+    // .keys is always set to a list of array keys.
+    // Length of list isn't always equal to .length.  Deleted elements stay
+    // as undefined and are part of the overall .length,
     // but aren't listed here.  This copies real array behavior.
     get keys() {
       const methods = ['length', 'push', 'pop', 'unshift', 'shift', 'Array', 'includes', 'indexOf', 'reverse', 'slice', 'map', 'filter', 'forEach', 'concat', 'find', 'findIndex', 'every', 'some'];
+
       return Object.keys(this).filter(item => (!methods.includes(item) && item != "keys")).sort(function(a, b) {
         // Sort by numeric vs alphabetic
         let out = isNaN(a) - isNaN(b);
         if(out != 0) {
           return out;
         }
-
+  
         // Subsort by either lexicographical or numerical
         if(a < b) {
           return -1;
@@ -73,20 +79,20 @@ const _Ray = function(arr) {
         }
       });
     },
-
+  
     // Returns contents as an Array object
     Array: function() {
       const out = new Array();
-
+  
       for(key in this) {
         out.push(this[key]);
       }
-
+  
       return out;
     },
-
+  
     // Stretch goals below
-
+  
     includes: function(item) {
       for(member of this.keys) {
         if(this[member] == item) {
@@ -95,7 +101,7 @@ const _Ray = function(arr) {
       }
       return false;
     },
-
+  
     indexOf: function(item) {
       for(member of this.keys) {
         if(this[member] == item) {
@@ -104,7 +110,7 @@ const _Ray = function(arr) {
       }
       return -1;
     },
-
+  
     reverse: function() {
       console.log("Keys: " + this.keys);
       for(let i = 0, j = this.length - 1; i < j; i++, j--) {
@@ -113,7 +119,7 @@ const _Ray = function(arr) {
         this[j] = tmp;
       }
     },
-
+  
     slice: function(from = 0, to = "end") {
       const out = Ray();
       if(to == "end") {
@@ -121,26 +127,26 @@ const _Ray = function(arr) {
       } else if(to < 1) {
         to = this.length + to;
       }
-
+  
       for(let i = from; i < to; i++) {
         out.push(this[i]);
       }
       return out;
     },
-
+  
     map: function(func, thisValue = undefined) {
       const out = Ray();
-
+  
       for(let i = 0; i < this.length; i++) {
         out[i] = func(this[i], i, this, thisValue);
       }
       console.log(typeof out);
       return out;
     },
-
+  
     filter: function(func, thisValue = undefined) {
       const out = Ray();
-
+  
       for(let i = 0; i < this.length; i++) {
         if(func(this[i], i, this, thisValue)) {
           out.push(this[i]);
@@ -148,34 +154,34 @@ const _Ray = function(arr) {
       }
       return out;
     },
-
+  
     forEach: function(func, thisValue = undefined) {
       const out = Ray();
-
+  
       for(let i = 0; i < this.length; i++) {
         if(this[i] != undefined) {
           func(this[i], i, this, thisValue);
         }
       }
     },
-
+  
     concat: function(array2) {
       const out = Ray();
-
+  
       for(let i = 0; i < this.length; i++) {
         out.push(this[i]);
       }
-
+  
       for(let i = 0; i < array2.length; i++) {
         out.push(array2[i]);
       }
-
+  
       return out;
     },
-
+  
     find: function(func, thisValue = undefined) {
       const out = Ray();
-
+  
       for(let i = 0; i < this.length; i++) {
         if(this[i] != undefined) {
           if(func(this[i], i, this, thisValue)) {
@@ -183,13 +189,13 @@ const _Ray = function(arr) {
           }
         }
       }
-
+  
       return undefined;
     },
-
+  
     findIndex: function(func, thisValue = undefined) {
       const out = Ray();
-
+  
       for(let i = 0; i < this.length; i++) {
         if(this[i] != undefined) {
           if(func(this[i], i, this, thisValue)) {
@@ -197,13 +203,13 @@ const _Ray = function(arr) {
           }
         }
       }
-
+  
       return -1;
     },
-
+  
     every: function(func, thisValue = undefined) {
       const out = Ray();
-
+  
       for(let i = 0; i < this.length; i++) {
         if(this[i] != undefined) {
           if(!func(this[i], i, this, thisValue)) {
@@ -211,13 +217,13 @@ const _Ray = function(arr) {
           }
         }
       }
-
+  
       return true;
     },
-
+  
     some: function(func, thisValue = undefined) {
       const out = Ray();
-
+  
       for(let i = 0; i < this.length; i++) {
         if(this[i] != undefined) {
           if(func(this[i], i, this, thisValue)) {
@@ -225,29 +231,60 @@ const _Ray = function(arr) {
           }
         }
       }
-
+  
       return false;
-    }
-  };
+    },
 
-  // Initialize array if array was provided
-  if(arr != undefined) {
-    let len = 0;
-    for(key in arr) {
-      out[key] = arr[key];
-      len++;
-    }
-    out.length = len;
-  }
-  return out;
-}
+    /*
+    *[Symbol.iterator] () {}
+      yield this[0];
+      yield this[1];
+      yield this[2];
+    } */
 
-// Create user-facing array object with proxy handler
-const Ray = function(arr) {
-  return new Proxy(_Ray(arr), {
+    [Symbol.iterator] () {
+      let step = 0;
+
+      iterate = this;
+
+      return {
+        next() {
+          if(step < iterate.length) {
+            return {done: false, value: iterate[step++]};
+          } else {
+            return {done: true};
+          }
+        }
+      }
+    },
+
+    valueOf: function() {
+      let out = "[ ";
+      for(element of this) {
+        if(typeof element == 'string') {
+          out += "'" + element + "', ";
+        } else {
+          out += element + ", ";
+        }
+      }
+      return out.slice(0, -2) + " ]";
+    },
+
+/* - Same as valueOf()
+    [Symbol.toPrimitive] () {
+      let out = "[ ";
+      for(element of this) {
+        if(typeof element == 'string') {
+          out += "'" + element + "', ";
+        } else {
+          out += element + ", ";
+        }
+      }
+      return out.slice(0, -2) + " ]";
+    }*/
+  }, {
     // Getter
     get: function(object, property) {
-      //console.log("property: " + property)
       return object[property];
     },
 
@@ -275,6 +312,8 @@ const Ray = function(arr) {
 
     deleteProperty: function(target, property) {
       if(!isNaN(property)) {
+        // If deleting a numeric property, set to undefined and keep it,
+        // behaving like real arrays do
         target[property] = undefined;
       } else {
         delete target[property];
